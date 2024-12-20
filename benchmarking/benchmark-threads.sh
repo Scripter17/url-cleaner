af5441103d79df93e9231e867a23f5a0e930f8a7
James C. Wise <jameschristopherwise@gmail.com>
2024-12-20 03:44:54 -0500

    Make benchmarking threads take much less time

--- benchmarking/benchmark-threads.sh
+++ benchmarking/benchmark-threads.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-THREAD_COUNTS=( 0 1 2 4 8 16 32 64 128 256 )
-THREAD_QUEUES=( 1 10 100 1000 10000 )
+THREAD_COUNTS=( 0 1 2 4 8 16 )
+THREAD_QUEUES=( 1 10 100 1000 )
 JSON=( "" --json )
 
 benchmark_args=( )
 url_cleaner_args=( )
+place=self
 mode=
 thread_counts_are_reset=0
 thread_queues_are_reset=0
@@ -16,20 +17,23 @@ now=$(date +%s)
 
 for arg in "$@"; do
   shift
-  case "$arg" in
-    --thread-counts) mode=thread_counts ;;
-    --thread-queues) mode=thread_queues ;;
-    --) case "$mode" in
-      benchmark_args) mode=url_cleaner_args ;;
-      url_cleaner_args) echo "Arg error."; exit 1 ;;
-      *) mode=benchmark_args; just_set_mode=1 ;;
+  case "$place" in
+    self) case "$arg" in
+      --thread-counts) mode=thread_counts ;;
+      --thread-queues) mode=thread_queues ;;
+      --) place=benchmark ;;
+      *) case "$mode" in
+        thread_counts) if [ $thread_counts_are_reset -eq 0 ]; then THREAD_COUNTS=( ); thread_counts_are_reset=1; fi; THREAD_COUNTS=( ${THREAD_COUNTS[@]} "$arg" ) ;;
+        thread_queues) if [ $thread_queues_are_reset -eq 0 ]; then THREAD_QUEUES=( ); thread_queues_are_reset=1; fi; THREAD_QUEUES=( ${THREAD_QUEUES[@]} "$arg" ) ;;
+      esac ;;
     esac ;;
-    *) case "$mode" in
-      "") echo "Arg error."; exit 1 ;;
-      benchmark_args) benchmark_args=( ${benchmark_args[@]} "$arg" ) ;;
-      url_cleaner_args) url_cleaner_args=( ${url_cleaner_args[@]} "$arg" ) ;;
-      thread_counts) if [ $thread_counts_are_reset -eq 0 ]; then THREAD_COUNTS=( ); thread_counts_are_reset=1; fi; THREAD_COUNTS=( ${THREAD_COUNTS[@]} "$arg" ) ;;
-      thread_queues) if [ $thread_queues_are_reset -eq 0 ]; then THREAD_QUEUES=( ); thread_queues_are_reset=1; fi; THREAD_QUEUES=( ${THREAD_QUEUES[@]} "$arg" ) ;;
+    benchmark) case "$arg" in
+      --) place=url_cleaner ;;
+      *) benchmark_args=( ${benchmark_args[@]} "$arg" )
+    esac ;;
+    url_cleaner) case "$arg" in
+      --) echo "Arg error."; exit 1 ;;
+      *) url_cleaner_args=( ${url_cleaner_args[@]} "$arg" )
     esac ;;
   esac
 done
